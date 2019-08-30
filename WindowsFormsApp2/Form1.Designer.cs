958439c7abfa7c19f5ae955eb636a416ff395b9e
Kostya_dev <serviceafcstudio@yandex.ru>
2019-08-30 16:57:02 +0500

    Добавьте файлы проекта.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 107);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 42);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.BorderColor = System.Drawing.Color.Silver;
+            chartArea1.Name = "ChartArea1";
+            chartArea1.ShadowColor = System.Drawing.Color.White;
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Location = new System.Drawing.Point(135, 12);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(893, 559);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1040, 583);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        List<double> MainData = new List<double>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public List<double> Load_TXT(string p)
+        {
+            List<string> fill = new List<string>();
+            using (StreamReader sc = new StreamReader(p))
+            {
+                sc.ReadLine();
+                while (!sc.EndOfStream)
+                {
+                    fill.Add(sc.ReadLine());
+                }
+            }
+            List<double> info2 = new List<double>();
+            for (int j = 0; j < fill.Count; j++)
+            {
+                info2.Add(Convert.ToDouble(fill[j].Split('-')[1].Replace(".", ",")));
+            }
+            return info2;
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            MainData = Load_TXT(@"C:\Users\kosty\OneDrive\Рабочий стол\Prediction-master\Данные\Сетевые-Атаки.txt");
+            Series series = new Series();
+            series.ChartType = SeriesChartType.Line;
+            foreach (var item in MainData)
+            {
+                series.Points.Add(item);
+            }
+            series.Color = Color.Blue;
+            chart1.Series.Add(series);
+
+            Series series2 = new Series();
+            series2.ChartType = SeriesChartType.Point;
+            foreach (var item in MainData)
+            {
+                series2.Points.Add(item);
+            }
+            series2.Color = Color.Blue;
+            chart1.Series.Add(series2);
+        }
+      
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            List<double> prediction = Prediction(MainData);
+
+            Series series = new Series();
+            series.ChartType = SeriesChartType.Line;
+            foreach (var item in prediction)
+            {
+                series.Points.Add(item);
+            }
+            series.Color = Color.Red;
+            chart1.Series.Add(series);
+
+
+            Series series2 = new Series();
+            series2.ChartType = SeriesChartType.Point;
+            foreach (var item in prediction)
+            {
+                series2.Points.Add(item);
+            }
+            series2.Color = Color.Red;
+            chart1.Series.Add(series2);
+        }
+
+
+        public List<double> Prediction(List<double> data)
+        {
+            List<double> prediction = new List<double>();
+
+            double y_mean = data.Average(x=>x); // Среднее значение массива
+
+            List <double> listA = new List<double>(); // Набор коэффициентов А по каждой гармоники
+            List<double> listB = new List<double>(); // Набор коэффициентов B по каждой гармоники
+
+            List<List<double>> harmonics = new List<List<double>>(); //Набор гармоник
+
+            for (int i = 0; i < data.Count / 2; i++) // Цикл гармоник
+            {
+                double a = 0; double b = 0; List<double> harmonic = new List<double>();
+                //---> Расчитать коэффициенты гармоники 
+                for (int j = 0; j < data.Count; j++) // Цикл перебора данных
+                {
+                    a = 2 * y_mean * (Math.Cos(2 * Math.PI * i * (j / data.Count)));
+                    b = 2 * y_mean * (Math.Sin(2 * Math.PI * i * (j / data.Count)));
+                    harmonic.Add((a * Math.Cos(2 * Math.PI * i * (j / data.Count))) + (b * Math.Sin(2 * Math.PI * i * (j / data.Count))));
+                }
+                listA.Add(a); listA.Add(b);
+                harmonics.Add(harmonic);
+                //---> Расчитать гармонику с коэффициентами 
+            }
+
+            for (int i = 0; i < data.Count / 2; i++)
+            {
+
+            }
+
+            //---> Суммирование гармоник 
+            return prediction;
+        }
+    }
+}
